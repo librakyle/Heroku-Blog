0cc9253f160e02ec8441ef507980f667b8cbb112
Kaihua Jiang <kaihua.jiang@hulu.com>
2013-06-04 13:28:31 +0800

    init blog with github and heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '90bb5f7472828cc5aedbc759be5d6c99648fb4eb9f3388059caebaf000f9bdd085c88c0ace2d4c14f11c541331067af69098120f1d1df2fad10d11d6cefb4c45'
